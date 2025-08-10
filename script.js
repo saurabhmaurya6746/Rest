1f75cfc6cf268b79502ffd7d4f1a7aad87a15228
Saurabh Maurya <saurabhmauryajnp28@gmail.com>
2025-08-10 21:12:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
       title: '🍔 Classic Cheeseburger',
       price: '₹199',
       desc: 'Juicy beef patty, melted cheese, fresh lettuce & tomato served on a toasted bun.',
-      model: 'assets/burger_tripo.glb',
+      model: 'assest/burger_tripo.glb',
       scale: '20 20 20',
       rotation: '0 180 0',
       thumb: 'assest/thumb_burger.jpg'
@@ -373,4 +373,5 @@ function closeModal() {
   renderMenu();
 
 });
+
 
